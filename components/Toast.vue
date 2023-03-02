cf9a039e48be04eef4c60380ed1408a8d6f0dbd3
Abdullahi Odesanmi <abdulodesanmi@gmail.com>
2023-03-02 10:29:39 +0100

    (feat): toast components

---
+++ components/Toast.vue
@@ -0,0 +1,103 @@
+<script lang="ts" setup>
+defineComponent({ name: 'Toast' });
+
+interface toastProps {
+    toastStyle: string;
+    type: string;
+    message: string;
+    description: string;
+}
+
+const props = defineProps<toastProps>();
+
+const borderColor = ref("");
+const color = ref("");
+
+type ColorMap = {
+    [key in string]: [string, string];
+};
+
+const background = computed(() => {
+    const colorMap: ColorMap = {
+        success: ['#00C271', '#FAFFFD'],
+        error: ['#F7002B', '#FFFAFB'],
+        warning: ['#FBBE37', '#FFFAFB'],
+        info: ['#6684FF', '#FBFCFF']
+    };
+    if (props.toastStyle === 'solid' || props.toastStyle === 'outline') {
+        const [bgColor, border] = colorMap[props.type];
+        borderColor.value = border;
+        color.value = "#FFFFFF";
+        if (props.toastStyle === 'outline') {
+            color.value = bgColor;
+            borderColor.value = bgColor;
+        }
+        return bgColor;
+    }
+});
+</script>
+
+<template>
+    <div class="toast"
+        :style="`border: ${toastStyle === 'outline' ? `1.6px solid ${borderColor}` : 'none'}; color: ${color}`">
+        <div class="toast__icon">
+            <IconsAlert :type="type" />
+        </div>
+        <div class="toast__content">
+            <p>{{ message }}</p>
+            <span>{{ description }}</span>
+        </div>
+        <button class="close-btn">
+            <IconsClose />
+        </button>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.toast {
+    width: 100%;
+    max-width: 350px;
+    padding: 20px;
+    border-radius: 20px;
+    background: v-bind(background);
+    display: flex;
+    align-items: center;
+    position: relative;
+
+    &__icon {
+        margin-right: 20px;
+        background: #ffffff26;
+        padding: 13px;
+        border-radius: 50%;
+    }
+
+    &__content {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+
+        p {
+            font-weight: 500;
+            font-size: 18px;
+            line-height: 22px;
+            margin-bottom: 5px;
+        }
+
+        span {
+            font-weight: 300;
+            font-size: 12px;
+            line-height: 14px;
+        }
+    }
+
+    .close-btn {
+        background: transparent;
+        border: none;
+        position: absolute;
+        right: 20px;
+        top: 20px;
+        cursor: pointer;
+        color: v-bind(color);
+    }
+}
+</style>
