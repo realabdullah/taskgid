145f9de0e70d665f0e8d1c447016d42fa26e295d
Abdullahi Odesanmi <abdulodesanmi@gmail.com>
2023-03-28 12:17:44 +0100

    (chore): installed pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: ["~/assets/scss/main.scss"],
-    modules: ['@nuxtjs/supabase'],
-    devServer: {
-        port: 1234
-    }
-})
+	css: ["~/assets/scss/main.scss"],
+	modules: ["@nuxtjs/supabase", "@pinia/nuxt"],
+	devServer: {
+		port: 1234,
+	},
+});
