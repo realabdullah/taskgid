5d2eeb0d98d1bb474f25a6b95cc3701b252b4879
Abdullahi Odesanmi <abdulodesanmi@gmail.com>
2023-03-28 12:19:04 +0100

    (feat): add tasks

---
+++ composables/useTasks.ts
@@ -0,0 +1,21 @@
+export const useTasks = () => {
+    const client = useSupabaseClient();
+    const user = useSupabaseUser();
+
+    const fetchUserTasks = async () => {
+        try {
+            const { data, error } = await client
+                .from("tasks")
+                .select("*")
+                .eq("user_id", user.value?.id);
+    
+            if (error) throw error;
+
+            return data as any;
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return { fetchUserTasks };
+};
