6e17a1bb815ce76a91be7da94ef4e09a16551e9f
realabdullah <abdulodesanmi@gmail.com>
2023-12-10 18:39:27 +0100

    debug

--- plugins/knock.client.ts
+++ plugins/knock.client.ts
@@ -4,28 +4,19 @@ export default defineNuxtPlugin(async () => {
 	const config = useRuntimeConfig();
 	const { user, notifications } = storeToRefs(useStore());
 
-	const knock = new Knock(config.public.knockKey);
-	knock.authenticate(user.value.email);
+	try {
+		const knock = new Knock(config.public.knockKey);
+		knock.authenticate(user.value.email);
 
-	const feedClient = knock.feeds.initialize(config.public.knockChannelId);
+		const feedClient = knock.feeds.initialize(config.public.knockChannelId);
 
-	feedClient.listenForUpdates();
+		feedClient.listenForUpdates();
 
-	const { data }: any = await feedClient.fetch({
-		status: "all",
-	});
+		const { data }: any = await feedClient.fetch({
+			status: "all",
+		});
 
-	notifications.value = data.entries.map((entry: any) => {
-		return {
-			id: entry.id,
-			message: entry.blocks[0].rendered,
-			date: entry.inserted_at,
-			read: entry.read_at !== null,
-		};
-	});
-
-	feedClient.on("items.received.*", ({ items }: any) => {
-		notifications.value = items.map((entry: any) => {
+		notifications.value = data.entries.map((entry: any) => {
 			return {
 				id: entry.id,
 				message: entry.blocks[0].rendered,
@@ -33,5 +24,18 @@ export default defineNuxtPlugin(async () => {
 				read: entry.read_at !== null,
 			};
 		});
-	});
+
+		feedClient.on("items.received.*", ({ items }: any) => {
+			notifications.value = items.map((entry: any) => {
+				return {
+					id: entry.id,
+					message: entry.blocks[0].rendered,
+					date: entry.inserted_at,
+					read: entry.read_at !== null,
+				};
+			});
+		});
+	} catch (error) {
+		console.error("Knock error: ", error);
+	}
 });
