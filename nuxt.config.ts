95dbc22d2c883705ae2e59ca22d167275427f85f
realabdullah <abdulodesanmi@gmail.com>
2025-04-19 22:54:31 +0100

    chore: update project structure and configuration
    - Added new files for app.vue, components.json, and tailwind.css.
    - Updated nuxt.config.ts to include new modules and configurations.
    - Removed unused files such as .npmrc and error.vue.
    - Enhanced .gitignore to include additional patterns.
    - Deleted pages/index.vue and server/api/join-waitlist.ts for cleanup.
    - Introduced utility functions in lib/utils.ts for class name management.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,31 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devServer: { port: 1234 }
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    'shadcn-nuxt',
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
